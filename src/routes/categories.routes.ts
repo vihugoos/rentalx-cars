a3022b63d809fed5f5309876279340926ef669d8
Victor Hugo <victorhugoos@live.com>
2022-09-22 19:44:25 -0300

    feat: created useCase importCategory

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const fileUpload = multer({
+    dest: "./tmp",
+});
 
 // Route to create a new category of cars
 categoriesRoutes.post("/", (request, response) => {
@@ -14,5 +20,14 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
 });
+
+// Route to import car categories file
+categoriesRoutes.post(
+    "/import",
+    fileUpload.single("file"),
+    (request, response) => {
+        return importCategoryController.handle(request, response);
+    }
+);
 
 export { categoriesRoutes };
