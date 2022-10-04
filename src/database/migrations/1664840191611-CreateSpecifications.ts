7b16ce4616eefaec6d562237f1367a645517d7dc
Victor Hugo <victorhugoos@live.com>
2022-10-04 19:12:00 -0300

    style: just formatting the code

--- src/database/migrations/1664840191611-CreateSpecifications.ts
+++ src/database/migrations/1664840191611-CreateSpecifications.ts
@@ -9,29 +9,20 @@ export class CreateSpecifications1664840191611 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-
                         type: "uuid",
-
                         isPrimary: true,
                     },
-
                     {
                         name: "name",
-
                         type: "varchar",
                     },
-
                     {
                         name: "description",
-
                         type: "varchar",
                     },
-
                     {
                         name: "created_at",
-
                         type: "timestamp",
-
                         default: "now()",
                     },
                 ],
