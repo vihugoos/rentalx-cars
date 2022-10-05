567999750c479677fc26afbc84c00be90ffc674e
Victor Hugo <victorhugoos@live.com>
2022-10-05 18:34:40 -0300

    feat: own error class to better handle erros

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 import "./shared/container";
 
+import { AppError } from "./modules/accounts/errors/AppError";
 import { routers } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -14,6 +16,22 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(routers);
+
+app.use(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message,
+            });
+        }
+
+        return response.status(500).json({
+            status: "error",
+            message: `Internal server error - ${err.message}`,
+        });
+    }
+);
 
 app.listen(3333, () => {
     console.log("\nHTTP Server is running on http://localhost:3333");
