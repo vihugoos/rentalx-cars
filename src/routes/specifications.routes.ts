f746270387e704bbabf82024980ef8ee66af93e2
Victor Hugo <victorhugoos@live.com>
2022-10-05 18:52:28 -0300

    feat: authenticated route

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../modules/accounts/middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/use-cases/specification/create-specification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-// Route to create a new specification
+// Route to create a new specification (authenticated)
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSpecificationController.handle);
 
 export { specificationsRoutes };
