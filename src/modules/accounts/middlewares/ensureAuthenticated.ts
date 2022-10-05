c54161260b083f6f97072d77d0bf803acf79e83b
Victor Hugo <victorhugoos@live.com>
2022-10-05 18:51:28 -0300

    feat: middleware to ensure authentication

---
+++ src/modules/accounts/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import { AppError } from "../errors/AppError";
+import { UsersRepository } from "../repositories/implementations/UsersRepository";
+
+interface IPayload {
+    sub: string;
+}
+
+export async function ensureAuthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+        throw new AppError("Token missing.", 401);
+    }
+
+    // Bearer 4NDk0MSic3VjoNjEfe
+    const [, token] = authHeader.split(" ");
+
+    try {
+        const { sub: user_id } = verify(
+            token,
+            "a978cb41d2b3e6a1a62cb2efbac8140f"
+        ) as IPayload;
+
+        const usersRepository = new UsersRepository();
+
+        const user = usersRepository.findById(user_id);
+
+        if (!user) {
+            throw new AppError("User does not exists.", 401);
+        }
+    } catch {
+        throw new AppError("Invalid token!", 401);
+    }
+
+    return next();
+}
