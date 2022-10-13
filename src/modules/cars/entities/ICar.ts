d766def2a37330b7422e36e38c4973462b8d352e
Victor Hugo <victorhugoos@live.com>
2022-10-13 19:00:37 -0300

    feat: entity ICar created

---
+++ src/modules/cars/entities/ICar.ts
@@ -0,0 +1,14 @@
+interface ICar {
+    id: string;
+    name: string;
+    description: string;
+    daily_rate: number;
+    available: boolean;
+    license_plate: string;
+    fine_amount: number;
+    brand: string;
+    category_id: string;
+    created_at: Date;
+}
+
+export { ICar };
