394e1882a0b0a96b9348b78207e6c8f5aba4d67c
Victor Hugo <victorhugoos@live.com>
2022-10-13 19:10:25 -0300

    feat: (TDD) use case to create a car

---
+++ src/modules/cars/use-cases/car/create-car/CreateCarUseCase.spec.ts
@@ -0,0 +1,66 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+    beforeEach(() => {
+        carsRepositoryInMemory = new CarsRepositoryInMemory();
+        createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+    });
+
+    it("Should be able to create a new car", async () => {
+        const car = await createCarUseCase.execute({
+            name: "Name Car",
+            description: "Description Car",
+            daily_rate: 100,
+            license_plate: "ABC-123",
+            fine_amount: 60,
+            brand: "Brand",
+            category_id: "ID-123",
+        });
+
+        expect(car).toHaveProperty("id");
+    });
+
+    it("Should not be able to create a car with an existing license plate", () => {
+        expect(async () => {
+            await createCarUseCase.execute({
+                name: "Car 1",
+                description: "Description Car",
+                daily_rate: 100,
+                license_plate: "ABC-123",
+                fine_amount: 60,
+                brand: "Brand",
+                category_id: "ID-123",
+            });
+
+            await createCarUseCase.execute({
+                name: "Car 2",
+                description: "Description Car",
+                daily_rate: 100,
+                license_plate: "ABC-123",
+                fine_amount: 60,
+                brand: "Brand",
+                category_id: "ID-123",
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("Should be able to create a car with available true by default", async () => {
+        const car = await createCarUseCase.execute({
+            name: "Car Available",
+            description: "Description Car",
+            daily_rate: 100,
+            license_plate: "ZZZ-999",
+            fine_amount: 60,
+            brand: "Brand",
+            category_id: "ID-123",
+        });
+
+        expect(car.available).toBe(true);
+    });
+});
