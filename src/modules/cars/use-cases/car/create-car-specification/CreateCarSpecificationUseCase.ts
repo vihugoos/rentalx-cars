ab9bf690cf2f53977700e424bfb51cee6919506f
Victor Hugo <victorhugoos@live.com>
2022-10-17 04:17:50 -0300

    feat: route to new specifications for the car

--- src/modules/cars/use-cases/car/create-car-specification/CreateCarSpecificationUseCase.ts
+++ src/modules/cars/use-cases/car/create-car-specification/CreateCarSpecificationUseCase.ts
@@ -1,3 +1,5 @@
+import { inject, injectable } from "tsyringe";
+
 import { ICar } from "@modules/cars/entities/ICar";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
@@ -8,9 +10,13 @@ interface IRequest {
     specifications_id: string[];
 }
 
+@injectable()
 class CreateCarSpecificationUseCase {
     constructor(
+        @inject("CarsRepository")
         private carsRepository: ICarsRepository,
+
+        @inject("SpecificationRepository")
         private specificationsRepository: ISpecificationsRepository
     ) {}
 
--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { CreateCarSpecificationController } from "@modules/cars/use-cases/car/create-car-specification/CreateCarSpecificationController";
 import { CreateCarController } from "@modules/cars/use-cases/car/create-car/CreateCarController";
 import { ListAvailableCarsController } from "@modules/cars/use-cases/car/list-available-cars/ListAvailableCarsController";
 import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
@@ -9,6 +10,7 @@ const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 // Route to create a new car
 carsRoutes.post(
@@ -20,5 +22,13 @@ carsRoutes.post(
 
 // Route to return all available cars
 carsRoutes.get("/available", listAvailableCarsController.handle);
+
+// Route to create a new specification to the car
+carsRoutes.post(
+    "/specifications/:car_id",
+    ensureAuthenticated,
+    ensureAdmin,
+    createCarSpecificationController.handle
+);
 
 export { carsRoutes };
