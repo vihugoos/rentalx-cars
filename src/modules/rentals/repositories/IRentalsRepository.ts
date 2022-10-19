0dfae434fa77295df4b7696acaedc722eda6d91b
Victor Hugo <victorhugoos@live.com>
2022-10-19 12:18:24 -0300

    feat: rentals repository interface

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,10 @@
+import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
+import { IRental } from "@modules/rentals/entities/IRental";
+
+interface IRentalsRepository {
+    create(data: ICreateRentalDTO): Promise<IRental>;
+    findOpenRentalByCar(car_id: string): Promise<IRental>;
+    findOpenRentalByUser(user_id: string): Promise<IRental>;
+}
+
+export { IRentalsRepository };
