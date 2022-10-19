0cc51636dfc0bfe560a7d4f87203cf488f138243
Victor Hugo <victorhugoos@live.com>
2022-10-19 12:14:39 -0300

    feat: IRental interface

---
+++ src/modules/rentals/entities/IRental.ts
@@ -0,0 +1,13 @@
+interface IRental {
+    id: string;
+    car_id: string;
+    user_id: string;
+    start_date: Date;
+    end_date: Date;
+    expected_return_date: Date;
+    total: number;
+    created_at: Date;
+    updated_at: Date;
+}
+
+export { IRental };
