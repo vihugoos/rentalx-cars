0046fcb34d380b4b476f5784a960f47649383be1
Victor Hugo <victorhugoos@live.com>
2022-10-19 19:11:29 -0300

    feat: typeorm implementation

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -1,16 +1,41 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryColumn,
+    UpdateDateColumn,
+} from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
 import { IRental } from "@modules/rentals/entities/IRental";
 
+@Entity("rentals")
 class Rental implements IRental {
+    @PrimaryColumn()
     id: string;
+
+    @Column()
     car_id: string;
+
+    @Column()
     user_id: string;
+
+    @Column()
     start_date: Date;
+
+    @Column()
     end_date: Date;
+
+    @Column()
     expected_return_date: Date;
+
+    @Column()
     total: number;
+
+    @CreateDateColumn()
     created_at: Date;
+
+    @UpdateDateColumn()
     updated_at: Date;
 
     constructor() {
