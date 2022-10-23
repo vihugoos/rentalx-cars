995ac8020a646af7a35ffcb6b5cd4b95ebe6cac0
Victor Hugo <victorhugoos@live.com>
2022-10-23 03:06:43 -0300

    test(CreateSpecificationController): new test for create specification controller

---
+++ src/modules/cars/use-cases/specification/create-specification/CreateSpecificationController.spec.ts
@@ -0,0 +1,53 @@
+import { hash } from "bcrypt";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm/";
+
+let connection: Connection;
+
+describe("Create Specification Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const id = uuidV4();
+        const password = await hash("admin_test", 8);
+
+        await connection.query(
+            ` INSERT INTO USERS(id, name, email, password, driver_license, "admin", created_at)
+                values('${id}', 'admin_test', 'admin@rentx.com', '${password}', 'XXX-XXX', true, 'now()')
+            `
+        );
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it("Should be able to create a new specification", async () => {
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentx.com",
+            password: "admin_test",
+        });
+
+        const { token } = responseToken.body;
+
+        const response = await request(app)
+            .post("/specifications")
+            .send({
+                name: "Specification Test",
+                description: "Specification test description",
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
+        expect(response.body.name).toEqual("Specification Test");
+    });
+});
