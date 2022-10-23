b345fd5936d83e07f3d2bfa2d1a051fe1ee78a91
Victor Hugo <victorhugoos@live.com>
2022-10-23 03:05:53 -0300

    test(CreateCategoryController): included new expect

--- src/modules/cars/use-cases/category/create-category/CreateCategoryController.spec.ts
+++ src/modules/cars/use-cases/category/create-category/CreateCategoryController.spec.ts
@@ -47,6 +47,7 @@ describe("Create Category Controller", () => {
             });
 
         expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
         expect(response.body.name).toEqual("Category Supertest");
         expect(response.body.description).toEqual("Category Supertest");
     });
