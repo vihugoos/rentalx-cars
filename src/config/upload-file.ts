e39d781bdd8d89f59bc187a8230936cdc1d1f738
Victor Hugo <victorhugoos@live.com>
2022-10-27 12:38:04 -0300

    refactor: upload function renamed to uploadFile

--- src/config/upload-file.ts
+++ src/config/upload-file.ts
@@ -3,7 +3,7 @@ import multer from "multer";
 import { resolve } from "path";
 
 export default {
-    upload(folder: string) {
+    uploadFile(folder: string) {
         return {
             storage: multer.diskStorage({
                 destination: resolve(__dirname, "..", "..", folder),
