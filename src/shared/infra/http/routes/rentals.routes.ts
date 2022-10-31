ca039ef342eb00608728f8294842741bade35661
Victor Hugo <victorhugoos@live.com>
2022-10-31 14:49:31 -0300

    feat: ListRentalsByUserController implemented

--- src/shared/infra/http/routes/rentals.routes.ts
+++ src/shared/infra/http/routes/rentals.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rentals/use-cases/create-rental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/use-cases/devolution-rental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/use-cases/list-rentals-by-user/ListRentalsByUserController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const rentalsRoutes = Router();
@@ -18,6 +19,13 @@ rentalsRoutes.post(
     "/devolution/:rental_id",
     ensureAuthenticated,
     new DevolutionRentalController().handle
+);
+
+// Routes to get all rentals by user
+rentalsRoutes.get(
+    "/user",
+    ensureAuthenticated,
+    new ListRentalsByUserController().handle
 );
 
 export { rentalsRoutes };
