9c3284b07b5c839e24efe0f0b175b94e8d61bbf1
Victor Hugo <victorhugoos@live.com>
2022-10-31 15:06:04 -0300

    feat: adjusted to bring the car data along with the rental in findByUser function

--- src/modules/rentals/infra/typeorm/entities/Rental.ts
+++ src/modules/rentals/infra/typeorm/entities/Rental.ts
@@ -2,17 +2,24 @@ import {
     Column,
     CreateDateColumn,
     Entity,
+    JoinColumn,
+    ManyToOne,
     PrimaryColumn,
     UpdateDateColumn,
 } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
 
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { IRental } from "@modules/rentals/entities/IRental";
 
 @Entity("rentals")
 class Rental implements IRental {
     @PrimaryColumn()
     id: string;
+
+    @ManyToOne(() => Car)
+    @JoinColumn({ name: "car_id" })
+    car: Car;
 
     @Column()
     car_id: string;
