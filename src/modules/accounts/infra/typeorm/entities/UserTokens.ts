0229726a0aae23736a415c5236ab93cebbae7486
Victor Hugo <victorhugoos@live.com>
2022-11-08 02:10:49 -0300

    feat: UsersTokensRepository implementation

---
+++ src/modules/accounts/infra/typeorm/entities/UserTokens.ts
@@ -0,0 +1,41 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { IUserTokens } from "@modules/accounts/entities/IUserTokens";
+
+import { User } from "./User";
+
+@Entity("users_tokens")
+class UserTokens implements IUserTokens {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    refresh_token: string;
+
+    @Column()
+    user_id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: "user_id" })
+    user: User;
+
+    @Column()
+    expires_date: Date;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        this.id = uuidV4();
+    }
+}
+
+export { UserTokens };
---
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -0,0 +1,42 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { UserTokens } from "@modules/accounts/infra/typeorm/entities/UserTokens";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+
+class UsersTokensRepository implements IUsersTokensRepository {
+    private repository: Repository<UserTokens>;
+
+    constructor() {
+        this.repository = getRepository(UserTokens);
+    }
+
+    async create({
+        user_id,
+        refresh_token,
+        expires_date,
+    }: ICreateUserTokenDTO): Promise<UserTokens> {
+        const newUserToken = this.repository.create({
+            user_id,
+            refresh_token,
+            expires_date,
+        });
+
+        const userToken = await this.repository.save(newUserToken);
+
+        return userToken;
+    }
+
+    async findByUserIdAndRefreshToken(
+        user_id: string,
+        refresh_token: string
+    ): Promise<UserTokens> {
+        return this.repository.findOne({ user_id, refresh_token });
+    }
+
+    async deleteById(id: string): Promise<void> {
+        await this.repository.delete(id);
+    }
+}
+
+export { UsersTokensRepository };
