016711dd2ecdfd408f255d078c71ddc8d2b4f102
Victor Hugo <victorhugoos@live.com>
2022-11-08 02:14:04 -0300

    feat: auth.ts for secret keys

---
+++ src/config/auth.ts
@@ -0,0 +1,9 @@
+export default {
+    secret_token: "a978cb41d2b3e6a1a62cb2efbac8140f",
+    expires_in_token: "15m",
+
+    secret_refresh_token: "0bd87e27f2b0b0b17bf1b8e674ef3a86",
+    expires_in_refresh_token: "30d",
+
+    expires_refresh_token_days: 30,
+};
