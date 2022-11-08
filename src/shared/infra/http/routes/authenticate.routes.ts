cd6c32f687095cbecffccec8e28f9fa218eb9908
Victor Hugo <victorhugoos@live.com>
2022-11-08 02:25:38 -0300

    feat: RefreshTokenController implemented

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
 import { AuthenticateUserController } from "@modules/accounts/use-cases/user/authenticate-user/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/use-cases/user/refresh-token/RefreshTokenController";
 
 const authenticateRoutes = Router();
 
 // Route to authenticate user
 authenticateRoutes.post("/sessions", new AuthenticateUserController().handle);
+
+// Route to update the refresh_token
+authenticateRoutes.post("/refresh-token", new RefreshTokenController().handle);
 
 export { authenticateRoutes };
