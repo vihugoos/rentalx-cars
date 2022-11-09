a4565f70fddb9c69388cff44bbf6e1fd8069835e
Victor Hugo <victorhugoos@live.com>
2022-11-09 16:46:08 -0300

    feat: MailProvider created

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,5 +2,12 @@ import { container } from "tsyringe";
 
 import { IDateProvider } from "./date-provider/IDateProvider";
 import { DayjsDateProvider } from "./date-provider/implementations/DayjsDateProvider";
+import { IMailProvider } from "./mail-provider/IMailProvider";
+import { EtherealMailProvider } from "./mail-provider/implementations/EtherealMailProvider";
 
 container.registerSingleton<IDateProvider>("DateProvider", DayjsDateProvider);
+
+container.registerInstance<IMailProvider>(
+    "MailProvider",
+    new EtherealMailProvider()
+);
