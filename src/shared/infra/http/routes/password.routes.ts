55ed40510f562636b551277f6279bc4ffb4a1e3f
Victor Hugo <victorhugoos@live.com>
2022-11-09 18:44:58 -0300

    feat: ResetPasswordUserController implemented

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 
+import { ResetPasswordUserController } from "@modules/accounts/use-cases/user/reset-password-user/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/use-cases/user/send-forgot-password-mail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 
+// Route to send an email with info for password recovery
 passwordRoutes.post("/forgot", new SendForgotPasswordMailController().handle);
+
+// Route to reset password
+passwordRoutes.post("/reset", new ResetPasswordUserController().handle);
 
 export { passwordRoutes };
