dda8c57f06ced341adb850dc6c9bf43e2d7c578e
Victor Hugo <victorhugoos@live.com>
2022-11-09 19:32:35 -0300

    refactor(auth.ts): capturing secret keys from .env

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,8 +1,8 @@
 export default {
-    secret_token: "a978cb41d2b3e6a1a62cb2efbac8140f",
+    secret_token: `${process.env.JWT_SECRET_TOKEN}`,
     expires_in_token: "15m",
 
-    secret_refresh_token: "0bd87e27f2b0b0b17bf1b8e674ef3a86",
+    secret_refresh_token: `${process.env.JWT_SECRET_REFRESH_TOKEN}`,
     expires_in_refresh_token: "30d",
 
     expires_refresh_token_days: 30,
