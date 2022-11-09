1b966ede9fe019d4027fd5f7bf5fec2222ba24b3
Victor Hugo <victorhugoos@live.com>
2022-11-09 16:43:35 -0300

    feat: new addHours function

--- src/shared/container/providers/date-provider/IDateProvider.ts
+++ src/shared/container/providers/date-provider/IDateProvider.ts
@@ -5,6 +5,7 @@ interface IDateProvider {
     diffInDays(date_1: Date, date_2: Date): number;
     todayAdd24Hours(): Date;
     addDays(days: number): Date;
+    addHours(hours: number): Date;
 }
 
 export { IDateProvider };
