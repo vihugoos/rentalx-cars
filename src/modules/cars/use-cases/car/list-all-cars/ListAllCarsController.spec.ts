89e9a1cd4a906c062dd3bdc0278674b35c6ae6c7
Victor Hugo <victorhugoos@live.com>
2022-11-18 19:20:05 -0300

    test: created ListAllCarsController.spec

---
+++ src/modules/cars/use-cases/car/list-all-cars/ListAllCarsController.spec.ts
@@ -0,0 +1,103 @@
+import { hash } from "bcrypt";
+import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
+import { AuthenticateUserUseCase } from "@modules/accounts/use-cases/user/authenticate-user/AuthenticateUserUseCase";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { CreateCarUseCase } from "@modules/cars/use-cases/car/create-car/CreateCarUseCase";
+import { DayjsDateProvider } from "@shared/container/providers/date-provider/implementations/DayjsDateProvider";
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm/";
+
+let usersRepository: UsersRepository;
+let usersTokensRepository: UsersTokensRepository;
+let dayjsDateProvider: DayjsDateProvider;
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let categoriesRepository: CategoriesRepository;
+let carsRepository: CarsRepository;
+let createCarUseCase: CreateCarUseCase;
+let connection: Connection;
+
+describe("List All Cars Controller", () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.dropDatabase();
+        await connection.runMigrations();
+
+        const id = uuidV4();
+        const password = await hash("admin_test", 8);
+
+        await connection.query(
+            ` INSERT INTO USERS(id, name, email, password, driver_license, "admin", created_at)
+                values('${id}', 'admin_test', 'admin@rentx.com', '${password}', 'XXX-XXX', true, 'now()')
+            `
+        );
+    });
+
+    beforeEach(() => {
+        usersRepository = new UsersRepository();
+        usersTokensRepository = new UsersTokensRepository();
+        dayjsDateProvider = new DayjsDateProvider();
+        authenticateUserUseCase = new AuthenticateUserUseCase(
+            usersRepository,
+            usersTokensRepository,
+            dayjsDateProvider
+        );
+        categoriesRepository = new CategoriesRepository();
+        carsRepository = new CarsRepository();
+        createCarUseCase = new CreateCarUseCase(carsRepository);
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
+    it("Should be able to list all cars", async () => {
+        const { refresh_token } = await authenticateUserUseCase.execute({
+            email: "admin@rentx.com",
+            password: "admin_test",
+        });
+
+        const category = await categoriesRepository.create({
+            name: "Category Test",
+            description: "Category test description",
+        });
+
+        await createCarUseCase.execute({
+            name: "Ferrari 488 Spider",
+            description: "Luxury sports",
+            daily_rate: 900,
+            license_plate: "FSW-2984",
+            fine_amount: 4000,
+            brand: "Ferrari",
+            category_id: category.id,
+        });
+
+        await createCarUseCase.execute({
+            name: "Lamborghini Aventador",
+            description: "Luxury sports",
+            daily_rate: 850,
+            license_plate: "FFFF-15544",
+            fine_amount: 4500,
+            brand: "Lamborghini",
+            category_id: category.id,
+        });
+
+        // List All Cars Controller (test ListAllCarsController)
+        const response = await request(app)
+            .get("/cars/all")
+            .set({
+                Authorization: `Bearer ${refresh_token}`,
+            });
+
+        expect(response.status).toBe(200);
+        expect(response.body.length).toBe(2);
+        expect(response.body[0].name).toEqual("Ferrari 488 Spider");
+        expect(response.body[1].name).toEqual("Lamborghini Aventador");
+    });
+});
