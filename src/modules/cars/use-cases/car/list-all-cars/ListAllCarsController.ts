f7772574c332f5f3b65bf10e2080299c45c27be0
Victor Hugo <victorhugoos@live.com>
2022-11-18 19:19:39 -0300

    feat: created ListAllCarsController

---
+++ src/modules/cars/use-cases/car/list-all-cars/ListAllCarsController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { ListAllCarsUseCase } from "./ListAllCarsUseCase";
+
+class ListAllCarsController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const listAllCarsUseCase = container.resolve(ListAllCarsUseCase);
+
+        const allCars = await listAllCarsUseCase.execute();
+
+        return response.json(allCars);
+    }
+}
+
+export { ListAllCarsController };
