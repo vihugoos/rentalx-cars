ed0906a0c152821eb396f8976b898a39a289527f
Victor Hugo <victorhugoos@live.com>
2022-11-19 22:34:22 -0300

    refactor(auth.ts): variables removed from .env

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,9 +1,9 @@
 export default {
-    secret_token: `${process.env.JWT_SECRET_TOKEN}`,
-    expires_in_token: "15m",
+    secret_token: "073cce591f4f6ee963c23e36b634b731",
+    expires_in_token: "10m",
 
-    secret_refresh_token: `${process.env.JWT_SECRET_REFRESH_TOKEN}`,
-    expires_in_refresh_token: "30d",
+    secret_refresh_token: "0bd87e27f2b0b0b17bf1b8e674ef3a86",
+    expires_in_refresh_token: "15d",
 
-    expires_refresh_token_days: 30,
+    expires_refresh_token_days: 15,
 };
