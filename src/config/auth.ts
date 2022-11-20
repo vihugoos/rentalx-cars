b6ccd4ccf51a42907cce0d95d4ec7c09a2db73e5
Victor Hugo <victorhugoos@live.com>
2022-11-20 00:44:49 -0300

    build: dotenv installed and configured

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,8 +1,10 @@
+import "dotenv/config";
+
 export default {
-    secret_token: "073cce591f4f6ee963c23e36b634b731",
+    secret_token: process.env.JWT_SECRET_TOKEN,
     expires_in_token: "10m",
 
-    secret_refresh_token: "0bd87e27f2b0b0b17bf1b8e674ef3a86",
+    secret_refresh_token: process.env.JWT_SECRET_REFRESH_TOKEN,
     expires_in_refresh_token: "15d",
 
     expires_refresh_token_days: 15,
