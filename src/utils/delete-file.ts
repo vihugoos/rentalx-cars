7ab1a9541623b975e03925b649ab76241f7cd3d6
Victor Hugo <victorhugoos@live.com>
2022-11-21 12:53:01 -0300

    refactor(delete-file.ts): adjusted deleteFile function

--- src/utils/delete-file.ts
+++ src/utils/delete-file.ts
@@ -1,12 +1,6 @@
 import fs from "fs";
 
-// eslint-disable-next-line consistent-return
 export const deleteFile = async (path: string) => {
-    try {
-        await fs.promises.stat(path);
-    } catch (error) {
-        return error.message;
-    }
-
+    await fs.promises.stat(path);
     await fs.promises.unlink(path);
 };
