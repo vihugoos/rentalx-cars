cba953fb57b4c5a169f39e7699342158fc9ad007
Victor Hugo <victorhugoos@live.com>
2022-11-21 12:49:56 -0300

    refactor(auth.ts): expires_in_token changed to 1d

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 
 export default {
     secret_token: process.env.JWT_SECRET_TOKEN,
-    expires_in_token: "10m",
+    expires_in_token: "1d",
 
     secret_refresh_token: process.env.JWT_SECRET_REFRESH_TOKEN,
     expires_in_refresh_token: "15d",
