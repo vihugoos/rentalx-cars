7460d2318d0316fd1002df909db952d4046ca636
Victor Hugo <victorhugoos@live.com>
2022-11-20 21:05:51 -0300

    refactor(UpdateUserAvatarUseCase): refactored to use StorageProvider

--- src/modules/accounts/use-cases/user/update-user-avatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/use-cases/user/update-user-avatar/UpdateUserAvatarUseCase.ts
@@ -2,30 +2,35 @@ import { inject, injectable } from "tsyringe";
 
 import { IUser } from "@modules/accounts/entities/IUser";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-import { deleteFile } from "@utils/delete-file";
+import { IStorageProvider } from "@shared/container/providers/storage-provider/IStorageProvider";
 
 interface IRequest {
     user_id: string;
-    avatar_file: string;
+    avatar_filename: string;
 }
 
 @injectable()
 class UpdateUserAvatarUseCase {
     constructor(
         @inject("UsersRepository")
-        private usersRepository: IUsersRepository
+        private usersRepository: IUsersRepository,
+
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
     ) {}
 
-    async execute({ user_id, avatar_file }: IRequest): Promise<IUser> {
+    async execute({ user_id, avatar_filename }: IRequest): Promise<IUser> {
         const user = await this.usersRepository.findById(user_id);
 
         if (user.avatar) {
-            await deleteFile(`./tmp/avatar/${user.avatar}`);
+            await this.storageProvider.delete(user.avatar, "avatar");
         }
+
+        await this.storageProvider.save(avatar_filename, "avatar");
 
         const userWithAvatar = await this.usersRepository.updateUserAvatar(
             user_id,
-            avatar_file
+            avatar_filename
         );
 
         return userWithAvatar;
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,10 +4,17 @@ import { IDateProvider } from "./date-provider/IDateProvider";
 import { DayjsDateProvider } from "./date-provider/implementations/DayjsDateProvider";
 import { IMailProvider } from "./mail-provider/IMailProvider";
 import { EtherealMailProvider } from "./mail-provider/implementations/EtherealMailProvider";
+import { LocalStorageProvider } from "./storage-provider/implementations/LocalStorageProvider";
+import { IStorageProvider } from "./storage-provider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>("DateProvider", DayjsDateProvider);
 
 container.registerInstance<IMailProvider>(
     "MailProvider",
     new EtherealMailProvider()
+);
+
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider",
+    LocalStorageProvider
 );
