c5ab419c52faef08c80eca75cc96c92e49e1743e
Victor Hugo <victorhugoos@live.com>
2022-11-20 21:02:31 -0300

    feat: LocalStorageProvider implemented

---
+++ src/shared/container/providers/storage-provider/implementations/LocalStorageProvider.ts
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { resolve } from "path";
+
+import upload from "@config/upload-file";
+
+import { IStorageProvider } from "../IStorageProvider";
+
+class LocalStorageProvider implements IStorageProvider {
+    async save(filename: string, folder: string): Promise<string> {
+        await fs.promises.rename(
+            resolve(upload.tmpFolder, filename),
+            resolve(`${upload.tmpFolder}/${folder}`, filename)
+        );
+
+        return filename;
+    }
+
+    async delete(filename: string, folder: string): Promise<void> {
+        const pathFile = resolve(`${upload.tmpFolder}/${folder}`, filename);
+
+        await fs.promises.stat(pathFile);
+        await fs.promises.unlink(pathFile);
+    }
+}
+
+export { LocalStorageProvider };
