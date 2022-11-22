59b51d94642bb1b381ef3e9c8e743b2bed0b9610
Victor Hugo <victorhugoos@live.com>
2022-11-22 14:30:36 -0300

    feat: ListCarImagesUseCase created

---
+++ src/modules/cars/use-cases/car/list-car-images/ListCarImagesUseCase.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from "tsyringe";
+
+import { ICarImageResponseDTO } from "@modules/cars/dtos/ICarImageResponseDTO";
+import { CarImageMap } from "@modules/cars/mapper/CarImageMap";
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+
+@injectable()
+class ListCarImagesUseCase {
+    constructor(
+        @inject("CarsImagesRepository")
+        private carsImagesRepository: ICarsImagesRepository
+    ) {}
+
+    async execute(car_id: string): Promise<ICarImageResponseDTO[]> {
+        const carImages = await this.carsImagesRepository.filterImagesByCarId(
+            car_id
+        );
+
+        const listCarImages = carImages.map((carImage) =>
+            CarImageMap.toDTO(carImage)
+        );
+
+        return listCarImages;
+    }
+}
+
+export { ListCarImagesUseCase };
