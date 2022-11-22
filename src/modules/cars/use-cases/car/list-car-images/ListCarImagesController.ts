fad90e76529cbb96a8ce46313a5059bad05babf3
Victor Hugo <victorhugoos@live.com>
2022-11-22 14:31:18 -0300

    feat: ListCarImagesController created

---
+++ src/modules/cars/use-cases/car/list-car-images/ListCarImagesController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { ListCarImagesUseCase } from "./ListCarImagesUseCase";
+
+class ListCarImagesController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { car_id } = request.params;
+
+        const listCarImagesUseCase = container.resolve(ListCarImagesUseCase);
+
+        const carImages = await listCarImagesUseCase.execute(car_id);
+
+        return response.json(carImages);
+    }
+}
+
+export { ListCarImagesController };
