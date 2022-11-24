6afda394e0d47cf37bef9d352aa768bbaf285a13
Victor Hugo <victorhugoos@live.com>
2022-11-23 23:09:04 -0300

    refactor: created a index container by provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,26 +1,3 @@
-import { container } from "tsyringe";
-
-import { IDateProvider } from "./date-provider/IDateProvider";
-import { DayjsDateProvider } from "./date-provider/implementations/DayjsDateProvider";
-import { IMailProvider } from "./mail-provider/IMailProvider";
-import { EtherealMailProvider } from "./mail-provider/implementations/EtherealMailProvider";
-import { LocalStorageProvider } from "./storage-provider/implementations/LocalStorageProvider";
-import { S3StorageProvider } from "./storage-provider/implementations/S3StorageProvider";
-import { IStorageProvider } from "./storage-provider/IStorageProvider";
-
-container.registerSingleton<IDateProvider>("DateProvider", DayjsDateProvider);
-
-container.registerInstance<IMailProvider>(
-    "MailProvider",
-    new EtherealMailProvider()
-);
-
-const diskStorage = {
-    local: LocalStorageProvider,
-    s3: S3StorageProvider,
-};
-
-container.registerSingleton<IStorageProvider>(
-    "StorageProvider",
-    diskStorage[process.env.DISK_STORAGE]
-);
+import "./date-provider";
+import "./mail-provider";
+import "./storage-provider";
