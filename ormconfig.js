07d8090e6d26e3c1674e2711626b987c91d61c94
Victor Hugo <victorhugoos@live.com>
2022-11-26 04:57:30 -0300

    fix(ormconfig): fixed file extension

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,5 @@
+const FILE_EXTENSION = process.env.BASE_FOLDER === "src" ? ".ts" : ".js";
+
 module.exports = {
     type: "postgres",
     host: "localhost",
@@ -5,8 +7,8 @@ module.exports = {
     database: process.env.DATABASE,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    migrations: [`./${process.env.BASE_FOLDER}/shared/infra/typeorm/migrations/*.ts`],
-    entities: [`./${process.env.BASE_FOLDER}/modules/**/entities/*.ts`],
+    migrations: [`./${process.env.BASE_FOLDER}/shared/infra/typeorm/migrations/*${FILE_EXTENSION}`],
+    entities: [`./${process.env.BASE_FOLDER}/modules/**/entities/*${FILE_EXTENSION}`],
     cli: {
         migrationsDir: `./${process.env.BASE_FOLDER}/shared/infra/typeorm/migrations/`,
     },
