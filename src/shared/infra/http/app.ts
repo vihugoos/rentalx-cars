f2f6fda968003ab20ffdf9182fb0c22f47ec5278
Victor Hugo <victorhugoos@live.com>
2022-11-29 01:24:47 -0300

    feat: cors installed and enabled

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "reflect-metadata";
 import "dotenv/config";
@@ -24,6 +25,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+
+app.use(cors());
 
 app.use(routers);
 
