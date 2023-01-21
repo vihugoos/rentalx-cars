6bf25d822fdf90aea5bfc81aa4f9b32583b03aba
Victor Hugo <victorhugoos@live.com>
2023-01-21 04:05:42 -0300

    refactor(rentals): adjusted IRentalRepository export

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,7 +1,7 @@
 import { ICreateRentalDTO } from "@modules/rentals/dtos/ICreateRentalDTO";
 import { IRental } from "@modules/rentals/entities/IRental";
 
-interface IRentalsRepository {
+export interface IRentalsRepository {
     create(data: ICreateRentalDTO): Promise<IRental>;
     findOpenRentalByCar(car_id: string): Promise<IRental>;
     findOpenRentalByUser(user_id: string): Promise<IRental>;
@@ -9,5 +9,3 @@ interface IRentalsRepository {
     finishRental(rental: IRental): Promise<IRental>;
     findByUser(user_id: string): Promise<IRental[]>;
 }
-
-export { IRentalsRepository };
